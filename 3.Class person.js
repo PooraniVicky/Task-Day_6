280f318c4656445bd4c9fc69b92bdd9fc8724988
PooraniVicky <154896129+PooraniVicky@users.noreply.github.com>
2024-01-05 23:28:08 +0530

    Add files via upload

---
+++ 3.Class person.js
@@ -0,0 +1,24 @@
+//Qn:3
+// Write a "Person" class to hold all the details.
+
+class Person {
+
+    constructor(name, age, occupation) {
+    
+    this.name = name;
+    
+    this.age = age;
+    
+    this.occupation = occupation;
+    
+    }
+    
+    }
+    
+    const person = new Person("Poorani", 25, "Trainer");
+    
+    console.log("Name:", person.name);
+    
+    console.log("Age:", person.age);
+    
+    console.log("Occupation:", person.occupation);
